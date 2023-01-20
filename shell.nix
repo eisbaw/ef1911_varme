dca90ead015c5e2195fd6ec2342099763e5f17a8
Mark Ruvald Pedersen <mped@demant.com>
2023-01-21 00:17:23 +0100

    nix: Fix python run-time error due to libstdc++

--- shell.nix
+++ shell.nix
@@ -6,12 +6,15 @@ with (import (builtins.fetchTarball {
 mkShell {
 
   buildInputs = [
+    stdenv.cc.cc.lib
     python39
     python39Packages.poetry
     python39Packages.pandas
   ];
 
   shellHook = ''
+   export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${stdenv.cc.cc.lib}/lib/"
+
    source $(poetry env info --path)/bin/activate
   '';
 }
